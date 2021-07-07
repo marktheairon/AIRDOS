7b3875f8da2875b96f30c22585a68add043a6e34
root <mark@theairon.com>
2021-07-07 02:23:18 +0000

    script edited

--- script/airdos.sh
+++ script/airdos.sh
@@ -7,8 +7,8 @@ ros2 run airdos waypoint_injector_client &
 
 sleep 3
 
-micrortps_agent -d /dev/ttyTHS0 -b 921600 &
+micrortps_agent -d /dev/ttyTHS1 -b 921600 &
 
 sleep 3
 
-ros2 run ros1_bridge daynamic_bridge &
+ros2 run ros1_bridge dynamic_bridge &
