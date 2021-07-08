3cd9a764961dcbe909a05d629cfc08bbf581129d
root <mark@theairon.com>
2021-07-08 02:39:43 +0000

    nano_run script

--- script/nano_run/airdos.sh
+++ script/nano_run/airdos.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-source /opt/ros/foxy/install/setup.bash ;
+source /opt/ros/dashing/setup.bash ;
 source /home/user/px4_ros_com_ros2/install/setup.bash ;
 
 ros2 run airdos waypoint_injector_client &
 
 sleep 3
 
-micrortps_agent -d /dev/ttyTHS0 -b 921600 &
+micrortps_agent -d /dev/ttyTHS1 -b 921600 &
 
 sleep 3
 
